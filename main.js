bfe8df618a8af0674de7596afbc58fdd42be0717
Mbalenhle Ndaba <mbalindaba01@gmail.com>
2022-06-06 13:22:55 +0200

    added deploy and config files

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 import './style.css'
 import Alpine from 'alpinejs'
 import {LoveCounter} from './love-counter';
-
-
+import Quotes from './quotes';
+import './quote.css'
 window.Alpine = Alpine
 
 Alpine.data('loveCounter', LoveCounter);
 
-Alpine.start()
+Alpine.data('quoteApp', Quotes)
 
+Alpine.start()
